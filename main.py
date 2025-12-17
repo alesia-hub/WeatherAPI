44d8a814c91b03996c9c593cddec7e88e791c235
alis_git <ra.perera15@gmail.com>
2025-12-17 17:24:31 +1100

    adding words definition part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -8,7 +9,11 @@ def weather():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", data="api data to display")
+
+@app.route("/word")
+def wird_definition():
+    return render_template("word.html")
 
 # now creating API:
 @app.route("/api/v1/<station>/<date>")
@@ -20,5 +25,18 @@ def api_weather(station, date):
         "condition": "Sunny"
     }
 
+@app.route("/api/v1/<word>")
+def api_definition(word):
+    response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    data = response.json()
+    if data:
+        definition = data[0]['meanings'][0]['definitions'][0]['definition']
+    else:
+        definition = "No definition found"
+    return {
+        "definition": definition,
+        "word": word
+    }
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
